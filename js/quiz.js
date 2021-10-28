407aa4b7d3afecbb06c2c51df1d705b42827bb02
DEEBIKA <deebikaece@gmail.com>
2021-10-28 14:27:39 +0530

    phase four project

---
+++ js/quiz.js
@@ -0,0 +1,54 @@
+let quiz=[
+    {
+        question:"Which HTML element do we put for the JavaScript?",
+        option:[
+            "1.<javasccript>",
+            "2.<>js>",
+            "3.<scripting>",
+            "4.<script>",
+        ],
+        answer:4,
+    },
+    {
+        question:"Syntax to include the external script xxx.js into the current project is",
+        option:[
+            "1. <script href=xxx.js>",
+            "2. <script name=xxx.js>",
+            "3. <script src=xxx.js>",
+            "4. <script id=xxx.js>",
+            
+        ],
+        answer:1,
+    },
+    {
+        question:"Which type the JavaScript language is?",
+        option:[
+            "1.Object-Oriented",
+            "2.Object-Based",
+            "3.Assembly-language",
+           "4.High-level",   
+        ],
+        answer:2,
+    },
+    {
+        question:"How to create a checkbox in HTML?",
+        option:[
+            '1.<input type = "checkbox">',
+            '2.<input type = "button">',
+            '3.<checkbox>',
+            '4.<input type = "check">',   
+        ],
+        answer:1,
+
+    },
+    {
+        question:"The function and var are known as",
+        option:[
+            "1.Keywords",
+            "2.Data types",
+            "3.Declaration statements",
+            "4.Prototypes",   
+        ],
+        answer:3,
+    }
+]
